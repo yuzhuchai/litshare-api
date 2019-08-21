f0a039651cd5360b11de9e99f32896b2381dafa1
Fatima Gomez <fatima3558@gmail.com>
2019-08-20 20:47:31 -0500

    app.py set up, ready to import models

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, g
+from flask_cors import CORS
+from flask_login import LoginManager
+import models
+
+#import blueprint here
+
+DEBUG = True
+PORT = 8000
+
+login_manager = LoginManager()
+
+app = Flask(__name__, static_url_path="")
+# secret_key
+login_manager.init_app(app)
+
+@login_manager.user_loader
+# load user method
+
+# set up CORS
+# set up controller
+
+# before_request decorator
+
+# after_request decorator
+
+@app.route('/')
+def index():
+	return 'connected'
+
+if __name__ == '__main__':
+	models.initialize()
+	app.run(debug=DEBUG, port=PORT)
